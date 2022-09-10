d3f4eb4af39ed126ad3d6537b3d8e91648d8e55d
hoanghuy1801 <hoanghuy.pham1801@gmail.com>
2022-09-11 00:11:06 +0700

    11/9/22

---
+++ src/configs/viewEngine.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const configViewEngine = (app) => {
+    app.set("view engine", "ejs");
+    app.set("views", "./src/views");
+    app.use(express.static('./src/public'))
+
+
+}
+
+export default configViewEngine;
---
+++ src/route/web.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import homeController from '../controller/homeController';
+
+let router = express.Router();
+
+const initWebRoute = (app) => {
+    router.get('/', homeController.getHomepage);
+    router.get('/about', (req, res) => {
+        res.send('Hoang Huy')
+    })
+
+    return app.use('/', router)
+
+}
+export default initWebRoute;
+
+
+
+
+
+
